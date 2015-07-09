ef6634f1e8d9d7a2e56934ff39d475904544d668
Damian Jimenez <jimenez.dmn@gmail.com>
2015-07-09 09:52:14 -0500

    Fixed some broken links

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,7 @@ jQuery(document).ready(function($) {
     /* jQuery RSS - https://github.com/sdepold/jquery-rss */
     $("#rss-feeds").rss(
         //Change this to your own rss feeds
-        "http://damianjimenez.tumblr.com/rss", {
+        "http://jimenezdmn.tumblr.com/rss", {
             // how many entries do you want?
             // default: 4
             // valid values: any integer
